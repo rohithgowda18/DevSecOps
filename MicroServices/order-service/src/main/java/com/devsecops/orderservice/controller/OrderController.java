f98b4f54e440a4fc77cfff7966c6d71bc82bc894
Rohithgowdak18 <rohithgowdak18@gmail.com>
2026-04-09 18:18:10 +0530

    test

--- MicroServices/order-service/src/main/java/com/devsecops/orderservice/controller/OrderController.java
+++ MicroServices/order-service/src/main/java/com/devsecops/orderservice/controller/OrderController.java
@@ -1,14 +1,19 @@
 package com.devsecops.orderservice.controller;
 
-import com.devsecops.orderservice.model.Order;
-import com.devsecops.orderservice.service.OrderService;
+import java.util.List;
+import java.util.Map;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
-import java.util.List;
-import java.util.Map;
+import com.devsecops.orderservice.model.Order;
+import com.devsecops.orderservice.service.OrderService;
 
 @RestController
 @RequestMapping("/orders")
@@ -31,4 +36,5 @@ public class OrderController {
         List<Order> orders = orderService.getAllOrders();
         return new ResponseEntity<>(orders, HttpStatus.OK);
     }
+    String Mongodb_password = "avvsiasuhauo";
 }
